99d2dd93d02bf2e0a0a2722f20ef31778672eb85
Pavlos Khetab <pavlos.khetab@gmail.com>
2020-07-16 10:49:32 +0100

    Formatted files to improve the layout of my code.

--- grip.js
+++ grip.js
@@ -5,26 +5,22 @@ function validateForm() {
   if (email == "" && badgeid == "") {
     alert("Email and Badge ID must be filled out");
     return false;
-  }
-  else if (email == "") {
+  } else if (email == "") {
     alert("Email must be filled out!");
     return false;
   }
-  // A regular expression to validate email address since input type = "email" returns true 
+  // A regular expression to validate email address since input type = "email" returns true
   // if you enter "aaaa@aaaa" which is invalid as email should end with a . followed by 2 or 3 characters
   // such as ".com"/".uk" etc.
-  
-  else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)== false)
-  {
-    alert("You have entered an invalid email address!")
+  else if (
+    /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email) == false
+  ) {
+    alert("You have entered an invalid email address!");
     return false;
-  } 
-  else if (badgeid == "") {
+  } else if (badgeid == "") {
     alert("Badge ID must be filled out!");
     return false;
-  }
-
-  else if (isNaN(badgeid)) {
+  } else if (isNaN(badgeid)) {
     alert("Please enter a number for Badge ID!");
   }
 }
